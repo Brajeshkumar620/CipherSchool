18fd6e7959be57ca61ef46cf8487127610376c7f
Brajesh Kumar <113361441+Brajeshkumar620@users.noreply.github.com>
2024-07-20 17:12:39 +0530

    Update provider.js

--- WEEK02-MERN-Lecture25/NODE/modulepattern/provider.js
+++ WEEK02-MERN-Lecture25/NODE/modulepattern/provider.js
@@ -1,1 +1,19 @@
 
+var counter=(arr)=>{
+    return"This array has" + arr.length+"items";
+}
+
+//es6 features
+let addition=(a, b) => 'The sum is ${a + b}';
+
+const constant=9.76543;
+
+// module.exports.counter=counter;
+// module.exports.addition=addition;
+// module.exports.constant=constant;
+
+module.exports={
+    counter:counter,
+    addition:addition,
+    constant: constant
+}
